6916c481e85629db3e165634ae655927e3c4e64e
antini <kakapukpuk16@gmail.com>
2026-04-27 10:23:24 +0300

    Delete mypackage directory

--- mypackage/utils/validators.py
+++
@@ -1,9 +0,0 @@
-def validate_student_name(first_name, last_name):
-    def is_valid_name(name):
-        return name and name.istitle() and name.replace('-', '').isalpha()
-
-    if not is_valid_name(first_name):
-        return False, "Имя должно начинаться с заглавной буквы и содержать только буквы или дефис."
-    if not is_valid_name(last_name):
-        return False, "Фамилия должна начинаться с заглавной буквы и содержать только буквы или дефис."
-    return True, "Имя и фамилия корректны."
